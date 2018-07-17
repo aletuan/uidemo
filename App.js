74236d32caf82f86266c15265efae4e6025f8a42
andy le <anhlt1983@gmail.com>
2018-07-17 21:42:07 +1000

    testing button

--- App.js
+++ App.js
@@ -96,6 +96,13 @@ export default class App extends Component {
                         <Badge success>
                             <Text>2</Text>
                         </Badge>
+                        <Button light><Text> Light </Text></Button>
+                        <Button primary><Text> Primary </Text></Button>
+                        <Button success><Text> Success </Text></Button>
+                        <Button info><Text> Info </Text></Button>
+                        <Button warning><Text> Warning </Text></Button>
+                        <Button danger><Text> Danger </Text></Button>
+                        <Button dark><Text> Dark </Text></Button>                        
                     </Content>
                     <Footer>
                         <FooterTab>
