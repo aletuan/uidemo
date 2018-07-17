4806ab81f2e6e6f3b082c8d0c3fd06adb945ea72
andy le <anhlt1983@gmail.com>
2018-07-17 23:02:21 +1000

    confirm with easy grid

--- App.js
+++ App.js
@@ -26,6 +26,8 @@ import {
     Badge,
 } from 'native-base';
 
+import { Cold, Row, Grid } from 'react-native-easy-grid';
+
 var BUTTONS = ['Option 0', 'Option 1', 'Option 2', 'Delete', 'Cancel'];
 var DESTRUCTIVE_INDEX = 3;
 var CANCEL_INDEX = 4;
@@ -70,52 +72,57 @@ export default class App extends Component {
                         </Right>
                     </Header>
                     <Content padder>
-                        <Accordion 
-                            dataArray={dataArray} 
-                            expanded={0} 
-                            icon="add" 
-                            expandedIcon="remove" 
-                            iconStyle={{ color: 'green' }}
-                            expandedIconStyle={{ color: 'red' }}
-                            headerStyle={{ backgroundColor: '#b7daf8' }}
-                            contentStyle={{ backgroundColor: '#ddecf8' }}
-                        />
-                        <Button
-                            iconLeft
-                            block                            
-                            onPress={() => 
-                                ActionSheet.show(
-                                    {
-                                        options: BUTTONS,
-                                        cancelButtonIndex: CANCEL_INDEX,
-                                        destructiveButtonIndex: DESTRUCTIVE_INDEX,
-                                        title: 'Testing ActionSheet'
-                                    },
-                                    buttonIndex => {
-                                        this.setState({ clicked: BUTTONS[buttonIndex] });
-                                    }                                
-                                )
-                            }
-                        >
-                            <Text>Actionsheet</Text>
-                            <Icon name='home' type='MaterialIcons' />
-                        </Button>
-                        <Badge>
-                            <Text>2</Text>
-                        </Badge>
-                        <Badge primary>
-                            <Text>2</Text>
-                        </Badge>
-                        <Badge success>
-                            <Text>2</Text>
-                        </Badge>
-                        <Button bordered rounded light><Text> Light </Text></Button>
-                        <Button bordered rounded primary><Text> Primary </Text></Button>
-                        <Button bordered rounded success><Text> Success </Text></Button>
-                        <Button bordered rounded info><Text> Info </Text></Button>
-                        <Button bordered rounded warning><Text> Warning </Text></Button>
-                        <Button bordered rounded danger><Text> Danger </Text></Button>
-                        <Button bordered rounded dark><Text> Dark </Text></Button>                        
+                        <Grid>
+                            <Row>
+                                <Accordion 
+                                    dataArray={dataArray} 
+                                    expanded={0} 
+                                    icon="add" 
+                                    expandedIcon="remove" 
+                                    iconStyle={{ color: 'green' }}
+                                    expandedIconStyle={{ color: 'red' }}
+                                    headerStyle={{ backgroundColor: '#b7daf8' }}
+                                    contentStyle={{ backgroundColor: '#ddecf8' }}
+                                />
+                            </Row>
+                            <Row>
+                                <Button
+                                    iconLeft
+                                    block                            
+                                    onPress={() => 
+                                        ActionSheet.show(
+                                            {
+                                                options: BUTTONS,
+                                                cancelButtonIndex: CANCEL_INDEX,
+                                                destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                                                title: 'Testing ActionSheet'
+                                            },
+                                            buttonIndex => {
+                                                this.setState({ clicked: BUTTONS[buttonIndex] });
+                                            }                                
+                                        )
+                                    }
+                                >
+                                    <Text>Actionsheet</Text>
+                                    <Icon name='home' type='MaterialIcons' />
+                                </Button>
+                            </Row>
+                            <Row>                                
+                                <Badge>
+                                    <Text>2</Text>
+                                </Badge>
+                                <Badge primary>
+                                    <Text>2</Text>
+                                </Badge>
+                                <Badge success>
+                                    <Text>2</Text>
+                                </Badge>
+                            </Row>
+                            <Row>
+                                <Button bordered rounded primary><Text> Primary </Text></Button>
+                                <Button bordered rounded success><Text> Success </Text></Button>
+                            </Row>
+                        </Grid>                       
                     </Content>
                     <Footer>
                         <FooterTab>
